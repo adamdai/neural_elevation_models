d6f8d83d69b11d48814fb91e5a57f5d2448c3c04
Adam Dai <adamdai97@gmail.com>
2024-03-14 19:44:15 -0700

    updated from laptop

--- nemo/global_planner.py
+++ nemo/global_planner.py
@@ -63,4 +63,65 @@ class GlobalPlanner(AStar):
         #ax.invert_xaxis()
         ax.grid(color='gray', linestyle='--', linewidth=0.5)
         return im
+    
+
+
+class GlobalPlanner2(AStar):
+
+    def __init__(self, costmat):
+        """
+        Parameters
+        ----------
+        costmap : CostMap
+
+        
+        """
+        self.costmat = costmat
+        self.costmat_dx = np.gradient(self.costmat, axis=0)
+        self.costmat_dy = np.gradient(self.costmat, axis=1)
+        self.width = self.costmat.shape[0]
+        self.height = self.costmat.shape[1]
+
+        self.path = None
+
+
+    def neighbors(self, node):
+        x, y = node
+        return [(nx, ny) for nx, ny in [(x+1, y), (x-1, y), (x, y+1), (x, y-1), (x+1, y+1), (x-1, y+1), (x+1, y-1), (x-1, y-1)] 
+                if 0 <= nx < self.width and 0 <= ny < self.height]
+
+    def distance_between(self, node1, node2):
+        return np.abs(self.costmat[node2] - self.costmat[node1]) 
+    
+    def heuristic_cost_estimate(self, node1, node2):
+        """Straight line distance"""
+        return np.linalg.norm(np.array(node1) - np.array(node2))
+    
+
+    def plan(self, start, goal):
+        """Find a path from start to goal
+        
+        Start and goal are in image coordinates
+        
+        """
+        path = list(self.astar(start, goal))  # path is in image coordinates
+        dt = np.dtype('int32','int32')
+        path = np.array(path, dtype=dt)[:,[1,0]]  # flip x and y to get back to spatial coordinates
+        self.path = path  
+        return path
+    
+
+    def plot(self, ax):
+        """Plot costmap and path"""
+        im = ax.imshow(self.costmat, cmap='viridis', origin='lower')
+        if self.path is not None:
+            ax.plot(self.path[:,0], self.path[:,1], 'r')
+            ax.scatter(self.path[:,0], self.path[:,1], c='r', s=10, marker='*')
+        # ax.set_xticks(np.arange(xmin, xmax, 50))
+        # ax.set_yticks(np.arange(ymin, ymax, 50))
+        # Invert x axis
+        #ax.invert_xaxis()
+        ax.grid(color='gray', linestyle='--', linewidth=0.5)
+        return im
+
 
