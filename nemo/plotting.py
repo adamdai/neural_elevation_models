011236d84d49740a6420d683b83c9678c25bbe5e
Adam Dai <adamdai97@gmail.com>
2024-05-11 08:44:09 -0700

    updates from ubuntu PC

--- nemo/plotting.py
+++ nemo/plotting.py
@@ -19,21 +19,24 @@ def plot_heatmap(data, fig=None, colorscale='Viridis', no_axes=False):
 
 ##### ------------------- 3D ------------------- #####
 
-def plot_surface(x, y, z, fig=None, colorscale='Viridis', no_axes=False):
+def plot_surface(x, y, z, fig=None, colorscale='Viridis', no_axes=False, showscale=True):
     if fig is None:
         fig = go.Figure()
-    fig.add_trace(go.Surface(x=x, y=y, z=z, colorscale=colorscale))
+    fig.add_trace(go.Surface(x=x, y=y, z=z, colorscale=colorscale, showscale=showscale))
     fig.update_layout(width=1200, height=900, scene_aspectmode='data')
     if no_axes:
         fig.update_layout(scene=dict(xaxis=dict(visible=False), yaxis=dict(visible=False), zaxis=dict(visible=False)))
     return fig
 
 
-def plot_path_3d(x, y, z, fig=None, color='red', markersize=3, linewidth=3):
+def plot_path_3d(x, y, z, fig=None, color='red', markersize=3, linewidth=3, markers=True, name=None):
     if fig is None:
         fig = go.Figure()
-    fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers+lines', marker=dict(size=markersize, color=color),
-                           line=dict(color=color, width=linewidth)))
+    if markers:
+        fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers+lines', marker=dict(size=markersize, color=color),
+                           line=dict(color=color, width=linewidth), name=name))
+    else:
+        fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='lines', line=dict(color=color, width=linewidth), name=name))
     return fig
 
 
