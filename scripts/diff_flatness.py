a153e2641981546ed9ec44c08bdd325d75933889
Adam Dai <adamdai97@gmail.com>
2024-06-19 10:37:29 -0700

    testing diff flatness backward pass

--- scripts/diff_flatness.py
+++ scripts/diff_flatness.py
@@ -52,9 +52,32 @@ phi = torch.arctan2(z_front - z_back, torch.tensor(l))
 g_eff = 9.81 * torch.sin(phi)   # when 9.81 is multipied, gradients become nan
 #g_eff = torch.sin(phi)         # without multiplying by 9.81, gradients are fine
 
+#%% Solve for control inputs
+
+u = torch.zeros(len(x)-2, 2)
+for i in range(len(x)-2):
+    J = torch.tensor([[torch.cos(theta[i]), -v[i] * torch.sin(theta[i])],
+                      [torch.sin(theta[i]), v[i] * torch.cos(theta[i])]], device=device, requires_grad=True)
+    b = torch.tensor([[xddot[i] + g_eff[i] * torch.cos(theta[i])],
+                      [yddot[i] + g_eff[i] * torch.sin(theta[i])]], device=device, requires_grad=True)
+    u[i] = torch.linalg.solve(J, b).flatten()
+
 # Formulate cost and compute gradients
-c = torch.sum(g_eff)
+c = torch.mean(u)
 c.backward()
 
 # Debugging: Check gradients
 print("path_opt.grad:", path_opt.grad)
+
+
+
+#%% Test linalg.solve backprop
+
+A = torch.tensor([[1.0, 2.0], [3.0, 4.0]], device=device, requires_grad=True)
+b = torch.tensor([1.0, 2.0], device=device, requires_grad=True)
+x = torch.linalg.solve(A, b)
+
+c = torch.sum(x)
+c.backward()
+
+print("A.grad:", A.grad)
